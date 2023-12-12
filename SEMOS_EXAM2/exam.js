3f355e39d0cad31623be37093c2678bcfbb86ad2
Ankica2023 <ankica.n@yahoo.com>
2023-12-12 22:23:47 +0100

    dodavanje exam2

---
+++ SEMOS_EXAM2/exam.js
@@ -0,0 +1,10 @@
+ var images=[ img,  img1, img2, img3, img4, imge5, img6, img7, img8, img9, img10]
+
+let img = document.getElementById('img')
+const change = id => {
+    img.src = '${id}.jpeg'
+    img.alt = '${id}.jpeg'
+
+}
+
+
